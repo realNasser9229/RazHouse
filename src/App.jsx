b74200ae3ef096ab453076a5335f67e37ada88e1
Nas9229alt <nassysussy@gmail.com>
2026-03-09 21:16:20 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'   // optional
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello from React on GitHub Pages 🔥</h1>
+      <p>Drop your old HTML here as JSX, my nigga. I'll convert it next if you send it.</p>
+      {/* Put all your old HTML converted to JSX right here */}
+    </div>
+  )
+}
+
+export default App
